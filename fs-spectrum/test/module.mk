90106b0e568965c8fae65d5f4fbdf96f27bd6edc
Dylan Harries <dylan.harries@adelaide.edu.au>
2014-10-16 18:10:16 +1030

    Added test cases for second derivatives of DR bar stop masses w.r.t. mu2(2,2)

--- fs-spectrum/test/module.mk
+++ fs-spectrum/test/module.mk
@@ -19,6 +19,7 @@ TEST_SRC := \
 		$(DIR)/test_lowE6SSM_stop_g1_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_g2_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_mq222_second_derivs.cpp \
+		$(DIR)/test_lowE6SSM_stop_mu222_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_vd_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_vu_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_vs_second_derivs.cpp \
@@ -103,6 +104,9 @@ $(DIR)/test_lowE6SSM_stop_g2_second_derivs.x: $(DIR)/test_lowE6SSM_stop_g2_secon
 		$(CXX) -o $@ $(call abspathx,$^) $(filter -%,$(LOOPFUNCLIBS)) $(BOOSTTESTLIBS) $(GSLLIBS) $(LAPACKLIBS) $(FLIBS) $(THREADLIBS)
 
 $(DIR)/test_lowE6SSM_stop_mq222_second_derivs.x: $(DIR)/test_lowE6SSM_stop_mq222_second_derivs.o $(LIBlowE6SSM) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))
+		$(CXX) -o $@ $(call abspathx,$^) $(filter -%,$(LOOPFUNCLIBS)) $(BOOSTTESTLIBS) $(GSLLIBS) $(LAPACKLIBS) $(FLIBS) $(THREADLIBS)
+
+$(DIR)/test_lowE6SSM_stop_mu222_second_derivs.x: $(DIR)/test_lowE6SSM_stop_mu222_second_derivs.o $(LIBlowE6SSM) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))
 		$(CXX) -o $@ $(call abspathx,$^) $(filter -%,$(LOOPFUNCLIBS)) $(BOOSTTESTLIBS) $(GSLLIBS) $(LAPACKLIBS) $(FLIBS) $(THREADLIBS)
 
 $(DIR)/test_lowE6SSM_stop_vd_second_derivs.x: $(DIR)/test_lowE6SSM_stop_vd_second_derivs.o $(LIBlowE6SSM) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))

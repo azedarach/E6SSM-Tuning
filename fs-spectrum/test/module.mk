cebfaae6ffd03c701e49e75a64ea3b66a2c5ccec
Dylan Harries <dylan.harries@adelaide.edu.au>
2014-10-16 17:45:53 +1030

    Added test cases for second derivatives of DR bar stop masses w.r.t. g1

--- fs-spectrum/test/module.mk
+++ fs-spectrum/test/module.mk
@@ -15,6 +15,7 @@ LIBTEST     := $(DIR)/lib$(MODNAME)$(LIBEXT)
 
 TEST_SRC := \
 		$(DIR)/test_lowE6SSM_stop_first_derivs.cpp \
+		$(DIR)/test_lowE6SSM_stop_g1_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_vd_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_vu_second_derivs.cpp \
 		$(DIR)/test_lowE6SSM_stop_vs_second_derivs.cpp \
@@ -86,6 +87,9 @@ clean:: clean-$(MODNAME)
 distclean:: distclean-$(MODNAME)
 
 $(DIR)/test_lowE6SSM_stop_first_derivs.x: $(DIR)/test_lowE6SSM_stop_first_derivs.o $(LIBlowE6SSM) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))
+		$(CXX) -o $@ $(call abspathx,$^) $(filter -%,$(LOOPFUNCLIBS)) $(BOOSTTESTLIBS) $(GSLLIBS) $(LAPACKLIBS) $(FLIBS) $(THREADLIBS) 
+
+$(DIR)/test_lowE6SSM_stop_g1_second_derivs.x: $(DIR)/test_lowE6SSM_stop_g1_second_derivs.o $(LIBlowE6SSM) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))
 		$(CXX) -o $@ $(call abspathx,$^) $(filter -%,$(LOOPFUNCLIBS)) $(BOOSTTESTLIBS) $(GSLLIBS) $(LAPACKLIBS) $(FLIBS) $(THREADLIBS) 
 
 $(DIR)/test_lowE6SSM_stop_vd_second_derivs.x: $(DIR)/test_lowE6SSM_stop_vd_second_derivs.o $(LIBlowE6SSM) $(LIBFLEXI) $(LIBLEGACY) $(filter-out -%,$(LOOPFUNCLIBS))
